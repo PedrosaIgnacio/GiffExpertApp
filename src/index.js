f7d9901c0f1b38a0cd8df73b55316090cb6dd6e0
Nacho <ignaciopedrosa1998@gmail.com>
2022-03-09 01:02:28 -0300

    fix pack.json

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import AddCategory from "./components/AddCategory";
 import GifExpertApp from "./GifExpertApp";
 import "./index.css";
 
